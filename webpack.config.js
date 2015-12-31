9e3c6f35d3e93f077a23548dfd30c92725cb4d4c
Chris Vodak <chris.vodak@mhdenterprises.com>
2015-12-31 11:11:57 -0600

    First few videos setup

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    context: __dirname + "/app",
+    entry: "./index.js",
+    output: {
+        path: __dirname + "/",
+        filename: "bundle.js",
+        loader: "babel-loader"
+    }
+};
