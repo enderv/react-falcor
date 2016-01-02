9f14950321c03f61ef9c40893dfa8f5f1c60dcd3
enderv <chris.vodak@gmail.com>
2016-01-02 10:20:39 -0600

    First falcor object

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const React = require('react');
 
+const model = new falcor.Model({
+  cache: {
+    recipes:[
+      {
+        name: "Cookies",
+        instructions: "Mix and bake"
+      },
+      {
+        name: "Brownies",
+        instructions: "Also bake them"
+      }
+    ]
+  }
+});
+
+//Only grab exactly what you need!!
+//Built to grab small pieces!
+model.get('recipes[0..1]["name","instructions"]')
+  .then(data => {
+    console.log(data);
+  })
+//this will also work!
+//model.get(['recipes',0,['name','instructions']]);
+
 const App = React.createClass({
   render() {
     return (<h1>hi lol</h1>);
